25698e730179020b3adb57ef3cdd2d5b81f6d2b3
BullfroG21 <gwechenberger.florian.public@gmail.com>
2016-05-26 18:07:48 +0200

    cd and history update
    cd: change dir got outsourced into a seperate funtion
    
    history: add printf with number and tab

--- bash_bts.c
+++ bash_bts.c
@@ -14,6 +14,9 @@
 #include <time.h>
 #include <dirent.h>
 
+#define ERROR_ALLOKATING (-1);
+#define ERROR_CHANGING_DIRECTORY (-2);
+
 struct tm *tmnow;
 char tasks[5][8] ={"fhsdate","fhstime","echo","help","exit"};
 char helps[5][100]={{"Befehl [fhsdate] gibt das Datum von heute aus"},
@@ -21,7 +24,6 @@ char helps[5][100]={{"Befehl [fhsdate] gibt das Datum von heute aus"},
                     {"Befehl [echo] gibt die nachstehenden Zeichen nochmal aus"},
                     {"Befehl [help] gibt alle Befehle aus"},
                     {"Befehl [exit] schließt das Programm"}};
-
 int main(void) {
 	 DIR * dir;
 	 struct dirent * currentdir;
@@ -31,10 +33,9 @@ int main(void) {
 	 //Fehlerkontrolle für fopen missing
 	 char * charpwd = malloc(sizeof(char)* 1024);
 	 if(charpwd == NULL){
-		 printf("Fehler beim allokieren!");
-		 return -1;
+		 return ERROR_ALLOKATING;
 	 }
-	 strcpy(charpwd,"/home/flo"); //copy of starting directory
+	 strcpy(charpwd,"/home/flo/"); //copy of starting directory
 	 int exit = 0; //0= false; 1=true for action "exit"
 	 //loop until user writes "exit"
 	 if ((dir = opendir (charpwd)) != NULL) {
@@ -57,128 +58,103 @@ int main(void) {
 
 		  //File * stream;
 		  fgets(input,1000, stdin);
-		  char input_history [strlen(input)];
-		  fseek(history_file,0,SEEK_SET);
-		  fscanf(history_file, "%i", &counter);
-		  counter++;
-		  char tmpzahl[10];
-		  sprintf(tmpzahl, "%i", counter);
-		  fseek(history_file,0,SEEK_SET);
-		  fprintf(history_file,tmpzahl);
-		  //strcpy(input_history,(char *)counter);
-		  fseek(history_file,0,SEEK_END);
-		  strcpy(input_history,input);
-		  fprintf(history_file,input_history);
+		  if(input[0]!='\n'){
 
 
+			  char input_history [strlen(input)];
+			  fseek(history_file,0,SEEK_SET);
+			  fscanf(history_file, "%i", &counter);
+			  counter++;
+			  char tmpzahl[10];
+			  sprintf(tmpzahl, "%i\n", counter);
+			  fseek(history_file,0,SEEK_SET);
+			  fprintf(history_file,tmpzahl);
+			  //strcpy(input_history,(char *)counter);
+			  fseek(history_file,0,SEEK_END);
+			  strcpy(input_history,input);
+			  fprintf(history_file,input_history);
 
-		  inputlength= strlen(input);
-		  realloc(input,strlen(input));
-		  int pos;
-		  pos = strcspn(input," ");
-		  //"Switch"
-		  if(pos<strlen(input)){
-			  attachment = 1;
-			  strcpy(arguments,input+pos+1);
-			  realloc(input,pos);
-			  input[pos]='\0';
-			  arguments[inputlength-pos-2]='\0';
-		  }
-		  else{
-			  input[inputlength-1]='\0';
-		  }
-		  //action echo
-		  if(strcmp(input,"echo")==0){
-			  printf("%s\n",arguments);
-		  }
-		  //action cd - change directory
-		  if(strcmp(input,"cd")==0){
-			  char * tmpstring = malloc(sizeof(char)*1024);
-			  if(tmpstring==0){
-				  printf("Fehler beim allokieren!");
-				  return -1;
+
+			  inputlength= strlen(input);
+			  realloc(input,strlen(input));
+			  int pos;
+			  pos = strcspn(input," ");
+			  //"Switch"
+			  if(pos<strlen(input)){
+				  attachment = 1;
+				  strcpy(arguments,input+pos+1);
+				  realloc(input,pos);
+				  input[pos]='\0';
+				  arguments[inputlength-pos-2]='\0';
 			  }
-			  strcpy(tmpstring,charpwd);
-			  //check which direction
-			  //checko on cd .. - return to one dir back
-			  if(strcmp(arguments,"..")==0){
-				  int tmppos = 0;
-				  //check every single position if there a the last valid position
-				  for(i=0;i<strlen(tmpstring)-1;i++){
-					  if(tmpstring[i] == '/'){
-						  tmppos = i;
-					  }
-				  }
-				  tmpstring[tmppos+1]='\0';
-			  }
-			  //check if requested whole path
-			  else if(arguments[0]=='/'){
-				  strcpy(tmpstring,arguments);
-			  }
-			  //add folder to path
 			  else{
-				  strcat(tmpstring,arguments);
+				  input[inputlength-1]='\0';
 			  }
-
-			  //check if last char == /, if not add /
-			  if(tmpstring[strlen(tmpstring)-1]!='/'){
-				  strcat(tmpstring,"/");
+			  //action echo
+			  if(strcmp(input,"echo")==0){
+				  printf("%s\n",arguments);
 			  }
-			  //check if path is reachable
-			  if ((dir = opendir (tmpstring)) != NULL) {
-			  		  strcpy(charpwd,tmpstring);
-			  	 }
-			  	 else{
-			  		  printf("Failed to open dir\n");
+			  //action cd - change directory
+			  else if(strcmp(input,"cd")==0){
+				  if(change_dir(strlen(charpwd),charpwd,strlen(arguments),arguments) == 0){
+					  printf("Path %s not existing!\n", arguments);
+				  }
 			  }
-			  free(tmpstring);
-		  }
-		  //action help
-		  else if(strcmp(input,"help")==0){
-			   int i;
-			   if(attachment==1){
-					for(i=0; i<(sizeof(tasks)/8);i++){
-						 if(strcmp(arguments,tasks[i])==0){
-							  printf("%s",helps[i]);
-							  break;
-						 }
+			  //action help
+			  else if(strcmp(input,"help")==0){
+				   int i;
+				   if(attachment==1){
+						for(i=0; i<(sizeof(tasks)/8);i++){
+							 if(strcmp(arguments,tasks[i])==0){
+								  printf("%s",helps[i]);
+								  break;
+							 }
+						}
+				   }
+				   else{
+					   for(i=0; i<(sizeof(tasks)/8);i++){
+						printf("-%s\n",tasks[i]);
 					}
-			   }
-			   else{
-				   for(i=0; i<(sizeof(tasks)/8);i++){
-					printf("-%s\n",tasks[i]);
-				}
-			   }
-		  }
-		  else if(strcmp(input,"pushd")==0){
-			  FILE * pushing;
-			  pushing = fopen("path.txt","w");
-			  fprintf(pushing, charpwd);
-			  fclose(pushing);
-		  }
-		  else if(strcmp(input,"history")==0){
-
-		  }
-		  //action fhsdate
-		  else if(strcmp(input,"fhsdate")==0){
-			 getdate();
-		  }
-		  //action fhstime
-		  else if(strcmp(input,"fhstime")==0){
-			 getTime();
-		  }
-		  else if(strcmp(input,"pwd")==0){
-			 printf("%s\n",charpwd);
-		  }
-		  //action exit
-		  else if(strcmp(input,"exit")==0){
-			 exit = 1;
-		  }
-		  else{
-			 printf("Unknown input: %s.\n", input);
+				   }
+			  }
+			  else if(strcmp(input,"pushd")==0){
+				  FILE * pushing;
+				  pushing = fopen("path.txt","w");
+				  fprintf(pushing, charpwd);
+				  fclose(pushing);
+			  }
+			  else if(strcmp(input,"history")==0){
+				  char * tmpstring = malloc(sizeof(char) * 1024);
+				  fseek(history_file,0,SEEK_SET);
+				  fgets(tmpstring,1024,history_file);
+				 // fscanf(config_file, "%*[^\n]\n", NULL);
+				  int i;
+				  for(i=0;i<counter-1;i++){
+					// printf("heho");
+				  	 fgets(tmpstring,1024,history_file);
+					 printf("%i\t%s",i+1,tmpstring);
+				  }
+			  }
+			  //action fhsdate
+			  else if(strcmp(input,"fhsdate")==0){
+				 getdate();
+			  }
+			  //action fhstime
+			  else if(strcmp(input,"fhstime")==0){
+				 getTime();
+			  }
+			  else if(strcmp(input,"pwd")==0){
+				 printf("%s\n",charpwd);
+			  }
+			  //action exit
+			  else if(strcmp(input,"exit")==0){
+				 exit = 1;
+			  }
+			  else{
+				 printf("Unknown input: %s.\n", input);
+			  }
 		  }
 	 }
-
 	 fclose(history_file);
 	 return 0;
 }
@@ -200,6 +176,52 @@ void getTime() {
    zeit = localtime(&sekunde);
    strftime(string, 80,"Es ist %H Uhr und %M Minuten",zeit);
    printf("%s\n",string);
+}
+
+	int change_dir(int dir_size, char charpwd[dir_size], int arg_size, char arguments[arg_size]){
+	DIR * dir;
+	char * tmpstring = malloc(sizeof(char)*1024);
+	if(tmpstring==0){
+	  return ERROR_ALLOKATING;
+	}
+	strcpy(tmpstring,charpwd);
+	//check which direction
+	//checko on cd .. - return to one dir back
+	if(strcmp(arguments,"..")==0){
+	  int tmppos = 0;
+	  //check every single position if there a the last valid position
+	  int i;
+	  for(i=0;i<strlen(tmpstring)-1;i++){
+		  if(tmpstring[i] == '/'){
+			  tmppos = i;
+		  }
+	  }
+	  tmpstring[tmppos+1]='\0';
+	}
+	//check if requested whole path
+	else if(arguments[0]=='/'){
+		strcpy(tmpstring,arguments);
+	}
+	//add folder to path
+	else{
+		strcat(tmpstring,arguments);
+	}
+
+	//check if last char == /, if not add /
+	if(tmpstring[strlen(tmpstring)-1]!='/'){
+	  strcat(tmpstring,"/");
+	}
+	//check if path is reachable
+	if ((dir = opendir (tmpstring)) != NULL) {
+		  strcpy(charpwd,tmpstring);
+		  free(tmpstring);
+		  return 1;
+
+	 }
+	 else{
+		  free(tmpstring);
+		  return 0;
+	}
 }
 
 void getecho(int size, char arguments[size]){
